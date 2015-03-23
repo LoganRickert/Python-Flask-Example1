15f4fa5f1e5547c955a5cb1d946fc05f43bff31e
Logan Rickert <logan@rickertcorp.com>
2015-03-23 10:31:54 -0400

    modified:   simple_app.py - Added clearn URL and an add page. 	The add page takes in two numbers and adds them together. They are 	converted in the inputs of the URL to integer. If a non-integer 	number is provided, the user is routed to a 404 page.

--- simple_app.py
+++ simple_app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import request
+# from flask import request
 
 app = Flask(__name__)
 
@@ -7,14 +7,27 @@ app = Flask(__name__)
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # http://localhost:8000/
 # Produces 'Hello from Logan'
-# http://localhost:8000/?name=Bob
+# http://localhost:8000/Bob
 # Produces 'Hello from Bob'
+# http://localhost:8000/add/5/5
+# Produces 10
+# http://localhost:8000/add/5/a
+# Produces a 404
 
 # Route is the everything after the http://~/
 # route('index') = http://~/index
+# <name> is a GET request that provides a clean URL.
 @app.route('/')
+@app.route('/<name>')
 def index(name="Logan"):
-	name = request.args.get('name', name) # If we get a name, great, if we don't, use the provided.
+	# name = request.args.get('name', name) # If we get a name, great, if we don't, use the provided. # Remvoed because the /<name> does this for us now.
 	return "Hello from {}".format(name)
 
+# You can convert things into ints inside the URL
+# The problem is that this produces a 404 if an integer is not provided.
+@app.route('/add/<int:num1>/<int:num2>')
+def add(num1, num2):
+	return "{} + {} = {}".format(num1, num2, num1 + num2)
+
+# Runs the app in debug mode. The server will restart after each save automatically.
 app.run(debug=True, port=8000, host='localhost')
