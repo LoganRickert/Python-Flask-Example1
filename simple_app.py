cfb22f9f66b2109c49a32ef155d6a869a1655e84
Logan Rickert <logan@rickertcorp.com>
2015-03-23 10:16:45 -0400

    modified: simple_app.py - Added GET request functionality

--- simple_app.py
+++ simple_app.py
@@ -1,9 +1,20 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
+# Test URLs
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# http://localhost:8000/
+# Produces 'Hello from Logan'
+# http://localhost:8000/?name=Bob
+# Produces 'Hello from Bob'
+
+# Route is the everything after the http://~/
+# route('index') = http://~/index
 @app.route('/')
-def index():
-	return "Hello from Logan!"
+def index(name="Logan"):
+	name = request.args.get('name', name) # If we get a name, great, if we don't, use the provided.
+	return "Hello from {}".format(name)
 
 app.run(debug=True, port=8000, host='localhost')
